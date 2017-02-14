621bd4ae9f2db0688381c9f885392122cc953735
Y-Chi Lu <luyege+github@gmail.com>
2017-02-13 22:36:02 -0600

    t

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require 'vendor/autoload.php';
 $app = new \Slim\App();
 // Define app routes
 $app->get('/message', function ($request, $response, $args) {
-    return $response->write("Hello ");
+    return $response->write("This is our messages ");
 });
 
 // Run app
