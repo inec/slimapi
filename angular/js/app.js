7af1a08cb9e4cc6d51b46a7cbe02c7a900982a8e
Y-Chi Lu <luyege+github@gmail.com>
2017-02-25 00:05:13 -0600

    Service 1

--- angular/js/app.js
+++ angular/js/app.js
@@ -2,9 +2,11 @@
 var app = angular.module('groceryListApp', []);//dependcy
 
 app.controller("HomeController", ["$scope", function($scope) {    $scope.appTitle = "Grocery List";
+
+    
 } ] );
 
-app.controller("GroceryListItemsController", ["$scope", function($scope){
+app.controller("GroceryListItemsController", ["$scope","Calc", function($scope,Calc){
     $scope.groceryItems = [
         {itemName: 'milk', date: '2014-10-01'},
         {itemName: 'cookies', date: '2014-10-01'},
@@ -14,7 +16,15 @@ app.controller("GroceryListItemsController", ["$scope", function($scope){
         {itemName: 'bread', date: '2014-10-03'},
         {itemName: 'eggs', date: '2014-10-04'},
         {itemName: 'tortillas', date: '2014-10-04'}
-    ]
+    ];
+
+
+   $scope.a={};
+   $scope.a.b=2;
+
+    $scope.timeTwo=function(){
+      $scope.a.b=Calc.timeTwo(   $scope.a.b);
+    };
 
 }])
 
@@ -24,4 +34,15 @@ app.directive("welcomeMessage",function(){
         restrict: "E",
         template:"<div>Howdy</div>"
     };
+});
+
+
+app.factory("Calc",function(){
+    
+    var calc={};
+    calc.timeTwo=function(a){
+        return a*2;
+    }
+
+    return calc;
 });
