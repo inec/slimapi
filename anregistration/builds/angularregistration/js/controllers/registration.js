81bd292ddb9c5492ef5b0c5992a81567c1a62995
Y-Chi Lu <luyege+github@gmail.com>
2017-03-20 20:34:42 -0500

    'notwork'

--- anregistration/builds/angularregistration/js/controllers/registration.js
+++ anregistration/builds/angularregistration/js/controllers/registration.js
@@ -1,19 +1,19 @@
 myApp.controller('RegistrationController', 
-  ['$scope', '$firebase','$firebaseAuth', 
-  function($scope, $firebase,$firebaseAuth) {
+  ['$scope', 'AuthService', 
+  function($scope, AuthService) {
 
 
 
 
 
   $scope.login = function() {
-
+      AuthService.login($scope.user);
   };
 
 
 
   $scope.register = function() {
-     
+     AuthService.register($scope.user);
 
    // Authentication.register($scope.user);
   }; //register
---
+++ anregistration/builds/angularregistration/js/services/authservice.js
@@ -0,0 +1,55 @@
+myApp.factory('Authentication',
+  ['$rootScope',  '$firebaseAuth',
+  function($rootScope, $firebaseAuth) {
+
+  var ref = firebase.database().ref();
+  var auth = $firebaseAuth();
+  var myObject;
+
+  auth.$onAuthStateChanged(function(authUser) {
+    if(authUser) {
+      var userRef = ref.child('users').child(authUser.uid);
+      var userObj = $firebaseObject(userRef);
+      $rootScope.currentUser = userObj;
+    } else {
+      $rootScope.currentUser = '';
+    }
+  });
+
+  myObject = {
+    login: function(user) {
+    $scope.message="wel"+$scope.user.email;
+      //signInWithEmailAndPassword
+    }, //login
+
+    logout: function() {
+      return auth.$signOut();
+    }, //logout
+
+
+
+    register: function(user) {
+      auth.$createUserWithEmailAndPassword(
+        user.email,
+        user.password
+      ).then(function(regUser) {
+        var regRef = ref.child('users')
+          .child(regUser.uid).set({
+            date: firebase.database.ServerValue.TIMESTAMP,
+            regUser: regUser.uid,
+            firstname: user.firstname,
+            lastname: user.lastname,
+            email: user.email
+          }); 
+          myObject.login(user);
+      }).catch(function(error) {
+        $rootScope.message = error.message;
+      }); //createUserWithEmailAndPassword
+    } //register
+
+  }; //return
+
+
+  return myObject;
+
+}]); //end of service factory
