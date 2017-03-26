3623acfe31f795b62478aad07dab4d532ac9e128
Y-Chi Lu <luyege+github@gmail.com>
2017-03-26 10:39:13 -0500

    'hang'

--- anregistration/builds/angularregistration/js/controllers/meetings.js
+++ anregistration/builds/angularregistration/js/controllers/meetings.js
@@ -9,7 +9,15 @@ myApp.controller('MeetingsController',
       if(authUser) {
         var meetingsRef = ref.child('users').child(authUser.uid).child('meetings');
         var meetingsInfo = $firebaseArray(meetingsRef);
-  
+              
+        $scope.addMeeting = function() {
+          meetingsInfo.$add({
+            name: $scope.meetingname,
+            date: firebase.database.ServerValue.TIMESTAMP
+          }).then(function() {
+            $scope.meetingname='';
+          }); //promise
+        } //addMeeting     
       }else{
       console.log("not auth");
       }//authUser
