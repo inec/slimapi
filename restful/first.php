cde99a727560f42afa424d39f1972cb116a8d98f
Y-Chi Lu <luyege+github@gmail.com>
2017-04-18 00:18:17 -0500

    't'

--- restful/first.php
+++ restful/first.php
@@ -1,2 +1,11 @@
 <?php
-require 'vendor/autoload.php';
+require 'vendor\autoload.php';
+
+use GuzzleHttp\Client;
+
+$Clent = new Client();
+
+$response =$client->request(
+'Get', 
+'http://jsonplaceholder.typicode.com/posts/1'
+	);
