0a70f0cbf10ad7589de6cda7be7fe230f126eb4b
Y-Chi Lu <luyege+github@gmail.com>
2017-04-18 22:24:10 -0500

    't'

--- restful/first.php
+++ restful/first.php
@@ -5,11 +5,14 @@ use GuzzleHttp\Client ;
 
 $client = new Client(['base_uri' =>'http://jsonplaceholder.typicode.com/']);
 
+$response =$client->get('posts/1'	);
+var_dump($response);
+echo $response->getBody();
 
-$response =$client->get(
+$response =$client->get('posts/2'	);
+var_dump($response);
+echo $response->getBody();
 
-'posts/1'
-	);
-
+$response =$client->get('comment/1'	);
 var_dump($response);
 echo $response->getBody();
