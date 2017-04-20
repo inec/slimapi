ca34c56c699ffe9870069394fa1ae049bf68bd7c
Y-Chi Lu <luyege+github@gmail.com>
2017-04-19 23:37:09 -0500

    'async'

---
+++ restful/async.php
@@ -0,0 +1,18 @@
+<?php
+require 'vendor\autoload.php';
+
+use GuzzleHttp\Client ;
+
+$client = new Client(['base_uri' =>'http://jsonplaceholder.typicode.com/']);
+
+$response =$client->get('posts/1'	);
+var_dump($response);
+echo $response->getBody();
+
+$response =$client->get('posts/2'	);
+var_dump($response);
+echo $response->getBody();
+
+$response =$client->get('http://httpbin.org/ip'	);
+var_dump($response);
+echo $response->getBody();
