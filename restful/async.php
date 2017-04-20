2eb40c0be6f59352f236319c7505e5cb9843a85e
Y-Chi Lu <luyege+github@gmail.com>
2017-04-19 23:42:04 -0500

    'async-first'

--- restful/async.php
+++ restful/async.php
@@ -5,14 +5,10 @@ use GuzzleHttp\Client ;
 
 $client = new Client(['base_uri' =>'http://jsonplaceholder.typicode.com/']);
 
-$response =$client->get('posts/1'	);
+$response =$client->request(
+    'GET',
+	'posts/1'
+	);
 var_dump($response);
 echo $response->getBody();
 
-$response =$client->get('posts/2'	);
-var_dump($response);
-echo $response->getBody();
-
-$response =$client->get('http://httpbin.org/ip'	);
-var_dump($response);
-echo $response->getBody();
