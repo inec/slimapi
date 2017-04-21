474ed86fcdfaf8f73d3a40fa31a632d1a60c5cb7
Y-Chi Lu <luyege+github@gmail.com>
2017-04-20 22:44:26 -0500

    'concur.php'

---
+++ restful/concur.php
@@ -0,0 +1,25 @@
+<?php
+require 'vendor\autoload.php';
+
+use GuzzleHttp\Client ;
+use GuzzleHttp\Psr7\Response ;
+use GuzzleHttp\Exception\RequestException ;
+
+$client = new Client(['base_uri' =>'http://jsonplaceholder.typicode.com/']);
+
+$promise =$client->requestAsync(
+    'GET',
+	'posts/1'
+);
+
+$promise -> then(
+		function (Response $resp)
+		{
+			echo $resp->getBody();
+		},
+		function (RequestException $re){
+			echo $e->getMessage();
+		}
+);
+
+$promise ->wait();
